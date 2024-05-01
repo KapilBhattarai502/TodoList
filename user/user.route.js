81008af64e40a59da2033fc686959b95b2f8d812
Kapil Bhattarai <kapilbhattarai@Kapils-MacBook-Pro.local>
2024-05-01 17:08:13 +0545

    using bcrypt for encrypting password

--- user/user.route.js
+++ user/user.route.js
@@ -1,5 +1,8 @@
 import express from "express";
 import { registerUserValidation } from "./user.validation.js";
+import {User} from "./user.model.js";
+import * as bcrypt from "bcrypt";
+
 const router=express.Router();
 router.post("/user/register",async(req,res,next)=>{
     //extract new user data from req body 
@@ -8,16 +11,29 @@ router.post("/user/register",async(req,res,next)=>{
     try{
         await registerUserValidation.validate(newUser)
     } catch(error){
+        // if validation fails,throw error 
         return res.status(400).send({message:error.message});
     }
     next();
 
-},(req,res)=>{
+},async(req,res)=>{
     
+    //extract new user data from req.body
+    const newUser=req.body;
+    //check if user with this email exists 
+    const user =await User.findOne({email:newUser.email});
 
-    
+    console.log(user);
+    if(user){
+        return res.status(409).send({message:"User with this email already exists"});
 
-    // if validation fails,throw error 
+    }
+    //hash password using bcrypt
+    const hashedPassword=await bcrypt.hash(newUser.password,10);
+    console.log(hashedPassword);
+
+    //create user
+
     return res.status(400).send({message:"user is registered suceessfully "});
 
 })
