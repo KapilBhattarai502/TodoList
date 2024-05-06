3209f081ec908c4e906c894de09492f35891021b
Kapil Bhattarai <kapilbhattarai502@gmail.com>
2024-05-06 14:28:32 +0545

    use of bcrypt

--- user/user.route.js
+++ user/user.route.js
@@ -1,7 +1,8 @@
 import express from "express";
-import { registerUserValidation } from "./user.validation.js";
+import { registerUserValidation,emailValidationSchema } from "./user.validation.js";
 import {User} from "./user.model.js";
 import * as bcrypt from "bcrypt";
+import * as Yup from "yup";
 
 const router=express.Router();
 router.post("/user/register",async(req,res,next)=>{
@@ -30,11 +31,50 @@ router.post("/user/register",async(req,res,next)=>{
     }
     //hash password using bcrypt
     const hashedPassword=await bcrypt.hash(newUser.password,10);
-    console.log(hashedPassword);
+    // console.log(hashedPassword);
+    newUser.password=hashedPassword;
 
     //create user
+    await User.create(newUser);
 
-    return res.status(400).send({message:"user is registered suceessfully "});
+    return res.status(201).send({message:"user is registered suceessfully "});
+
+})
+
+router.post("/user/login",async(req,res,next)=>{
+
+    // console.log(req.body);
+    const loginCredentials= req.body;
+    //validate email 
+    try{
+       
+        await emailValidationSchema.validate(loginCredentials.email)
+
+    }catch(error){
+        return res.status(400).send({message:error.message});
+
+    }
+
+
+return res.status(200).send({message:"Logged in Sucessfully"});
+next()},async(req,res)=>{
+    //extract Login Credentials from req.body 
+    const loginCredentials=req.body;
+    //check if email matches 
+    const user= await User.findOne({email:loginCredentials.email});
+    // if not user,throw error 
+    if(!user){
+        return res.status(404).send({message:"Invalid Credentials"})
+    }
+    //check for password matches or not 
+    const passwordMatch = await bcrypt .compare(loginCredentials.password,user.password);
+    console.log(passwordMatch);
+    if(!passwordMatch){
+        return res.status(404).send({message:"Invalid Credentials"})
+    }
+    user.password=undefined;
+    return res.status(200).send({message:"Logged In Successfully"});
+
 
 })
 export default router;
--- user/user.validation.js
+++ user/user.validation.js
@@ -1,11 +1,14 @@
 import * as Yup from "yup"
-import dayjs from "dayjs";
+
 export const registerUserValidation=Yup.object({
     firstName:Yup.string().max(55,"first name must be at max 55 characters").trim().required("first name is required"),
     lastName:Yup.string().max(20,"last name must be at max 55 characters").trim().required("last name is required"),
-    email:Yup.string().trim().required("Email is required").max(60,"Email must be at most 60 characters").lowercase(),
+    email:Yup.string().trim().required("Email is required").max(60,"Email must be at most 60 characters").lowercase().email(),
     password:Yup.string().min(4,"Password must be at max 4 characters ").max(16,"Password must be at max 16 characters").required(),
-    gender:Yup.string().oneOf(["male","female","preferNotToSay"]).nullable().trim().lowercase(),
+    gender:Yup.string().oneOf(["male","female","preferNotToSay"]).nullable().trim(),
     // description:Yup.string().required("Description is required.".trim().max(57,"Description must be at most 57 characters")),
   
+
+
 })
+export const emailValidationSchema=Yup.string().email("Must be a valid email").trim().lowercase();
